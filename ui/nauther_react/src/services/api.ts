1a5e75f0984b26dbe26eac71753f2a6709689e3d
Mohammad_Yousefiyan <yousefiyan.mohammad@maxasecurity.com>
2025-06-07 11:56:21 +0330

    api.ts

--- ui/nauther_react/src/services/api.ts
+++ ui/nauther_react/src/services/api.ts
@@ -31,23 +31,25 @@ api.interceptors.response.use(
 );
 // Request interceptor to add token
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('accesstoken');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token =JSON.parse(sessionStorage.getItem("oidc.default")).tokens.accessToken;
+  console.log("Token from sessionStorage:", token);
+  // const token = localStorage.getItem('accesstoken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 }, (error) => {
-    return Promise.reject(error);
+  return Promise.reject(error);
 });
 
 // Response interceptor to handle errors
 api.interceptors.response.use((response) => {
-    return response;
+  return response;
 }, (error) => {
-    if (error.response?.status === 401) {
-        localStorage.removeItem("accesstoken");
-        window.location.href = '/login';
-    }
-    return error;
+  if (error.response?.status === 401) {
+    localStorage.removeItem("accesstoken");
+    window.location.href = '/login';
+  }
+  return error;
 });
 export default api;
