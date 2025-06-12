0f5bdffd9388c22ba93b736634c3a58b01904dbc
Mohammad_Yousefiyan <yousefiyan.mohammad@maxasecurity.com>
2025-06-13 01:28:08 +0330

    [api]:migration files

---
+++ api/Identity/Nauther.Identity.Persistence/Migrations/20250612105107_RolePermission_Edit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nauther.Identity.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolePermission_Edit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "RolePermissions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "RolePermissions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "RolePermissions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "RolePermissions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "RolePermissions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "RolePermissions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "RolePermissions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UpdatedBy",
+                table: "RolePermissions",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+    }
+}
---
+++ api/Identity/Nauther.Identity.Persistence/Migrations/20250612110013_UserPermission_UserRole_Edit.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nauther.Identity.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPermission_UserRole_Edit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "UserPermissions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "UserPermissions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UserPermissions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "UserPermissions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserRoles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "UserRoles",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UserRoles",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UpdatedBy",
+                table: "UserRoles",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserPermissions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "UserPermissions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UserPermissions",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UpdatedBy",
+                table: "UserPermissions",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+    }
+}
